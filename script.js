df3e519f5a68498dc9fb0cecc18e7068ee7537b0
kubapolomski <polomski.j@gmail.com>
2020-04-03 14:46:24 +0200

    Work in progress...

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
+// Made by Kuba Polomski. Uncopyrighted (you can use it anywhere you want)
 
 window.addEventListener('scroll', showBacktoTop);
 let startArrow = document.getElementsByClassName('go-start')[0];
-let documentSize = document.body.offsetHeight - window.innerHeight;
 
 function showBacktoTop() {
     if (window.pageYOffset < 300) {
-        startArrow.classList.add('hidden') ;    
-    } else if (window.pageYOffset == documentSize) {
-        startArrow.classList.add('hidden') ;    
+        startArrow.classList.add('hidden') ;     
     } else {
         startArrow.classList.remove('hidden') 
     }
